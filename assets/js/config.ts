571c493115120f1cdd8156cfbed06696b60a104b
Evgeny Stepanovych <undsoft@gmail.com>
2021-07-07 22:29:52 +0300

    * Pointing dev to dev API

--- assets/js/config.ts
+++ assets/js/config.ts
@@ -1,5 +1,12 @@
-export const config = {
+const config = {
   baseUrl: 'https://truepool.io/v1',
   daysForChart: 60,
   startingDay: new Date(2021, 7-1, 2),
 };
+
+// TODO: Do something better
+if (window.location.href !== 'truepool.io') {
+  config.baseUrl = 'https://dev.truepool.io/v1'
+}
+
+export { config };
