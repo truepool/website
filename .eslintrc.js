082d26495a2a8b87e31155bda1a654fde4885fc8
Evgeny Stepanovych <undsoft@gmail.com>
2021-07-18 11:22:06 +0300

    #22 - Adding linter

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  }
+};
