18856f585004989365b998061f0935967ffa45ee
Aaron Ervin <aervin@ixsystems.com>
2021-07-26 17:18:41 -0500

    import and var cleanup

--- src/app/pages/stats-page/payout-search/payout-search.store.ts
+++ src/app/pages/stats-page/payout-search/payout-search.store.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
-import { subHours } from 'date-fns';
-import { EMPTY, forkJoin, Observable } from 'rxjs';
-import { catchError, map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { Payout } from 'src/app/interfaces/payout.interface';
 import { PayoutService } from 'src/app/services/api/payout.service';
 
@@ -20,9 +19,6 @@ const initialState: PayoutSearchState = {
 
 @Injectable({ providedIn: 'root' })
 export class PayoutSearchStore extends ComponentStore<PayoutSearchState> {
-  readonly maxSearchResults = 5;
-  readonly partialsForLastHours = 24;
-
   constructor(private payoutService: PayoutService) {
     super(initialState);
   }
