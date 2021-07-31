5d5513f936bda1579cfc96f482247cb27ab2f7cf
Aaron Ervin <aaron.jeffrey.ervin@gmail.com>
2021-07-31 12:27:06 -0500

    whitespace

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from "@angular/core";
 import { ContentLoaderComponent } from "./components/content-loader/content-loader.component";
 import { XchPipe } from "./pipes/xch.pipe";
 
-const COMPONENTS = [ContentLoaderComponent]
-const PIPES = [XchPipe]
+const components = [ContentLoaderComponent]
+const pipes = [XchPipe]
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [...COMPONENTS, ...PIPES],
-    exports: [CommonModule, ...COMPONENTS, ...PIPES]
+    declarations: [...components, ...pipes],
+    exports: [CommonModule, ...components, ...pipes]
 })
 export class SharedModule {}
