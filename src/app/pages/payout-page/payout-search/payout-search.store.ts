0ac8fa21680643623586fee63189fa8ca06ae0f7
Aaron Ervin <aaron.jeffrey.ervin@gmail.com>
2021-08-07 11:28:51 -0500

    Squashed commit of the following:
    commit 66bed119df072e795384432eee1c078e2ffdcc10
    Author: Aaron Ervin <aaron.jeffrey.ervin@gmail.com>
    Date:   Sat Aug 7 11:26:52 2021 -0500
    
        filter out null farmers in payout list; move some deps into shared module; add pool details bar to payout page

--- src/app/pages/payout-page/payout-search/payout-search.store.ts
+++ src/app/pages/payout-page/payout-search/payout-search.store.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { combineLatest, EMPTY, Observable } from 'rxjs';
-import { catchError, switchMap, takeUntil, tap } from 'rxjs/operators';
+import {
+  catchError, switchMap, takeUntil, tap,
+} from 'rxjs/operators';
 import { CoinRecord } from 'src/app/interfaces/coin-record.interface';
 import { Payout, PayoutAddress } from 'src/app/interfaces/payout.interface';
 import { CoinRecordService } from 'src/app/services/api/coin-record.service';
-import { FarmerService } from 'src/app/services/api/farmer.service';
 import { PayoutService } from 'src/app/services/api/payout.service';
 
 export interface PayoutSearchState {
@@ -21,12 +22,12 @@ const initialState: PayoutSearchState = {
   error: null,
   payout: null,
   addresses: null,
-  coinRecords: null
+  coinRecords: null,
 };
 
 @Injectable({ providedIn: 'root' })
 export class PayoutSearchStore extends ComponentStore<PayoutSearchState> {
-  constructor(private payoutService: PayoutService, private coinRecordService: CoinRecordService, private farmerService: FarmerService) {
+  constructor(private payoutService: PayoutService, private coinRecordService: CoinRecordService) {
     super(initialState);
   }
 
@@ -38,22 +39,24 @@ export class PayoutSearchStore extends ComponentStore<PayoutSearchState> {
           isLoading: true,
         });
       }),
-      switchMap((payoutId: string) =>
-        combineLatest([
-          this.payoutService.getPayout(payoutId)
-            .pipe(tap((payout) => { this.patchState({ payout }) })),
-          this.coinRecordService.getCoinRecords({ payout: payoutId })
-            .pipe(
-              tap((coinRecords) => {
-                this.patchState({ coinRecords: coinRecords.results });
-              })
-            ),
-          this.payoutService.getPayoutAddresses({ payout: payoutId })
-            .pipe(
-              tap((addresses) => { this.patchState({ addresses: addresses.results }); })
-            )
-          ])
-      ),
+      switchMap((payoutId: string) => combineLatest([
+        this.payoutService.getPayout(payoutId)
+          .pipe(tap((payout) => { this.patchState({ payout }); })),
+        this.coinRecordService.getCoinRecords({ payout: payoutId })
+          .pipe(
+            tap((coinRecords) => {
+              this.patchState({ coinRecords: coinRecords.results });
+            }),
+          ),
+        this.payoutService.getPayoutAddresses({ payout: payoutId })
+          .pipe(
+            tap((addresses) => {
+              this.patchState({
+                addresses: addresses.results.filter((address) => address.farmer !== null),
+              });
+            }),
+          ),
+      ])),
       tap(() => { this.patchState({ isLoading: false }); }),
       catchError(() => {
         const error = 'Error when searching for a payout.';
@@ -66,7 +69,7 @@ export class PayoutSearchStore extends ComponentStore<PayoutSearchState> {
 
         return EMPTY;
       }),
-      takeUntil(this.destroy$)
+      takeUntil(this.destroy$),
     );
-  })
+  });
 }
