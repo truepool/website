3260815e0e1bff33450f6dcb744d9a091a678ea3
Aaron Ervin <aaron.jeffrey.ervin@gmail.com>
2021-08-19 18:03:44 -0500

    more boilerplate for farmers page

--- src/app/pages/farmer-page/farmer/farmer.component.ts
+++ src/app/pages/farmer-page/farmer/farmer.component.ts
@@ -7,12 +7,14 @@ import { FarmerStore } from '../farmer.store';
 @UntilDestroy(this)
 @Component({
   selector: 'app-farmer',
+  styleUrls: ['./farmer.component.scss'],
   templateUrl: './farmer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FarmerComponent {
   launcherId$ = this.ar.paramMap.pipe(map((pMap) => pMap.get('launcherId')));
-  farmer$ = this.farmerStore.state$.pipe(
+  state$ = this.farmerStore.state$;
+  farmer$ = this.state$.pipe(
     filter((state) => !state.isLoading),
     map((state) => state.farmer),
   );
