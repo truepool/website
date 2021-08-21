ebcae98494d0a40a70f2608192a5a7b5e54d2160
Jarett Langton <84741515+feydkin@users.noreply.github.com>
2021-08-20 19:13:43 -0700

    requested changes fixed

--- src/app/components/ce-block/ce-block.component.ts
+++ src/app/components/ce-block/ce-block.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ce-block',
   templateUrl: './ce-block.component.html',
-  styleUrls: ['./ce-block.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CeBlockComponent {
   @Input() blockId: string | number;
