83043c4b277c643ae6eacfc2a19b2cd17959df28
Jarett Langton <84741515+feydkin@users.noreply.github.com>
2021-08-20 19:05:13 -0700

    requested changes

--- src/app/pages/stats-page/coin-table/coin-table.component.ts
+++ src/app/pages/stats-page/coin-table/coin-table.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CoinRecordsStore } from '../coin-records.store';
 
 @Component({
   selector: 'app-coin-table',
   templateUrl: './coin-table.component.html',
   styleUrls: ['./coin-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoinTableComponent implements OnInit {
   coinRecordsState$ = this.coinRecordsStore.state$;
