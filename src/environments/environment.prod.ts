1a8a770f1a0abbf130fca9cf2929eb82fabcbb61
Kris Moore <kris@ixsystems.com>
2021-10-01 14:44:06 -0400

    Cleanup old proxy setup, and point dev environment at correct API endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiRoot: 'http://localhost:4200/api/v1.0',
+  apiRoot: 'https://truepool.io/v1/pool',
   daysForChart: 60,
   startingDay: new Date(2021, 7 - 1, 2),
 };
