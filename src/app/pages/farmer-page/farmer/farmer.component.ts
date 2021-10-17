142036fe6c63c928d267a5ef488758c2bd85a06f
Aaron Ervin <aaron.jeffrey.ervin@gmail.com>
2021-10-17 12:26:05 -0500

    add farmer page links to coinrecord table

--- src/app/pages/farmer-page/farmer/farmer.component.ts
+++ src/app/pages/farmer-page/farmer/farmer.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { map, take } from 'rxjs/operators';
 import { FarmerPartial } from 'src/app/interfaces/farmer-partial.interface';
@@ -21,13 +21,23 @@ export class FarmerComponent {
       : null)),
   );
 
-  constructor(private ar: ActivatedRoute, private store: FarmerSearchStore) {}
+  constructor(
+    private ar: ActivatedRoute,
+    private store: FarmerSearchStore,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.ar.paramMap
       .pipe(map((pMap) => pMap.get('launcherId')))
       .pipe(take(1))
       .subscribe(this.store.searchFarmer);
+
+    this.state$.subscribe((state) => {
+      if (!state.isLoading && state.results.length === 0) {
+        void this.router.navigateByUrl('/404');
+      }
+    });
   }
 
   filterErrors(partials: FarmerPartial[]): FarmerPartial[] {
